5b31f3c956dda2a4d1c72d13fb6725fdaef87685
Daniel Ellis <daniel.ellis@ext.esa.int>
2024-08-06 21:42:13 +0100

    elastic

--- cmipld/generate/elasticmipcvs.py
+++ cmipld/generate/elasticmipcvs.py
@@ -61,6 +61,7 @@ async def main():
         counter = 0
         
         for entry in data:
+            print(entry["@id"],entry) 
             if '@id' in entry:
                 es.index(index=index, id=entry["@id"], body=entry)
                 counter += 1
