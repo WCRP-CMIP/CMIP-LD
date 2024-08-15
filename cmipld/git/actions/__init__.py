ad90ae5357241f636d42bfc9bd500d59c261c089
Daniel Ellis <daniel.ellis@ext.esa.int>
2024-08-15 23:02:35 +0100

    git repo

--- cmipld/git/actions/__init__.py
+++ cmipld/git/actions/__init__.py
@@ -4,8 +4,12 @@ from . import add
 
 
 def update_env(key,value):
-   
-    github_env_file = "/home/runner/work/_temp/_runner_file/_github_env"  # Temporary path used by GitHub Actions
+    import os
+    github_env_file = os.environ.get("GITHUB_ENV") 
+    print(github_env_file)
+    
+    
+    # "/home/runner/work/_temp/_runner_file/_github_env"  # Temporary path used by GitHub Actions
 
     with open(github_env_file, "a") as env_file:
         env_file.write(f"{key}={value}\n")
