4d1b66138c2f07a1c9972945eb763bf0d90e616b
Daniel Ellis <daniel.ellis@ext.esa.int>
2024-11-20 20:52:46 +0000

    development

--- cmipld/utils/urltools.py
+++ cmipld/utils/urltools.py
@@ -1,5 +1,7 @@
 import requests
 # import urlparse
+from urllib.parse import urlparse, urlunparse
+from os.path import relpath
 
 def url_exists(url):
     try:
@@ -13,7 +15,24 @@ def url_exists(url):
         print(f"An error occurred: {e}")
         return None
     
+
+
+def relative_url(base_url, target_url):
+    base_parsed = urlparse(base_url)
+    target_parsed = urlparse(target_url)
+
+    if base_parsed.netloc != target_parsed.netloc:
+        raise ValueError("URLs have different domains; cannot compute relative path.")
+
+    # Compute relative path
+    base_path = base_parsed.path
+    target_path = target_parsed.path
+    relative_path = relpath(target_path, start=base_path)
     
+    # Reconstruct the relative URL
+    return relative_path
+
+
 # def valid_url(url):
 #     parsed_url = urlparse(url)
 #     # Check if the URL has a valid scheme (e.g., 'http', 'https') and netloc (domain)
