10708b4c3570f68e49498051ef81ee91e387e5de
Daniel Ellis <daniel.ellis@ext.esa.int>
2024-12-02 19:32:01 +0000

    development

--- static/treemap/index.js
+++ static/treemap/index.js
@@ -5,116 +5,84 @@ const height = window.innerHeight;
 fetch("https://wcrp-cmip.github.io/LD-Collection/universe_contents/universe_hierarchy.json")
   .then((response) => response.json())
   .then((data) => {
-      // Specify the chart’s dimensions.
-  const width = 928;
-  const height = 660;
+    const colors = d3.schemeCategory10.map((color) =>
+      d3.scaleSequential([5, 0], (t) => d3.interpolateRgb(color, "white")(t))
+    );
 
-  let colours = d3.schemeCategory10.map((d) =>
-    d3.scaleSequential([5, 0], d3.interpolate(d, "white"))
-  );
+    const colorMap = {};
+    data.children.forEach((d, i) => (colorMap[d.prefix] = colors[i]));
 
-  let color = {};
-  data.children.forEach((d, i) => (color[d.prefix] = colours[i]));
-  console.log(color);
-  // const color = d3.scaleSequential([8, 0], d3.interpolate("red", "white"));
-
-  // Create the treemap layout.
-  const treemap = (data) =>
-    d3
-      .treemap()
-      .size([width, height])
-      .paddingOuter(3)
-      .paddingTop(19)
-      .paddingInner(1)
-      .round(true)(
+    const treemap = (data) =>
       d3
-        .hierarchy(data)
-        .sum((d) => d.size)
-        .sort((a, b) => b.size - a.size)
-    );
-  const root = treemap(data);
+        .treemap()
+        .size([width, height])
+        .paddingOuter(3)
+        .paddingTop(19)
+        .paddingInner(1)
+        .round(true)(
+        d3
+          .hierarchy(data)
+          .sum((d) => d.size)
+          .sort((a, b) => b.size - a.size)
+      );
 
-  // Create the SVG container.
-  const svg = d3
-    .create("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", [0, 0, width, height])
-    .attr(
-      "style",
-      "max-width: 100%; height: auto; overflow: visible; font: 10px sans-serif;"
-    );
+    const root = treemap(data);
 
-  const shadow = DOM.uid("shadow");
+    const svg = d3
+      .create("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", `0 0 ${width} ${height}`)
+      .attr(
+        "style",
+        "max-width: 100%; height: auto; overflow: visible; font: 10px sans-serif;"
+      );
 
-  svg
-    .append("filter")
-    .attr("id", shadow.id)
-    .append("feDropShadow")
-    .attr("flood-opacity", 0.3)
-    .attr("dx", 0)
-    .attr("stdDeviation", 3);
+    const shadow = svg.append("filter").attr("id", "shadow");
+    shadow
+      .append("feDropShadow")
+      .attr("flood-opacity", 0.3)
+      .attr("dx", 0)
+      .attr("stdDeviation", 3);
 
-  const node = svg
-    .selectAll("g")
-    .data(d3.group(root, (d) => d.height))
-    .join("g")
-    .attr("filter", shadow)
-    .selectAll("g")
-    .data((d) => d[1])
-    .join("g")
-    .attr("transform", (d) => `translate(${d.x0},${d.y0})`);
+    const node = svg
+      .selectAll("g")
+      .data(root.descendants())
+      .join("g")
+      .attr("transform", (d) => `translate(${d.x0},${d.y0})`);
 
-  const format = d3.format(",d");
-  node.append("title").text(
-    (d) =>
-      `${d
-        .ancestors()
-        .reverse()
-        .map((d) => d.data.name)
-        .join("/")}\n${format(d.value)}`
-  );
+    const format = d3.format(",d");
 
-  node
-    .append("rect")
-    .attr("id", (d) => (d.nodeUid = DOM.uid("node")).id)
-    .attr("fill", (d) =>
-      d.depth > 0 ? color[d.data.prefix](d.height) : "white"
-    )
-    .attr("width", (d) => d.x1 - d.x0)
-    .attr("height", (d) => d.y1 - d.y0);
+    node
+      .append("title")
+      .text(
+        (d) =>
+          `${d
+            .ancestors()
+            .reverse()
+            .map((d) => d.data.name)
+            .join("/")}\n${format(d.value)}`
+      );
 
-  node
-    .append("clipPath")
-    .attr("id", (d) => (d.clipUid = DOM.uid("clip")).id)
-    .append("use")
-    .attr("xlink:href", (d) => d.nodeUid.href);
+    node
+      .append("rect")
+      .attr("fill", (d) =>
+        d.depth > 0 ? colorMap[d.data.prefix](d.height) : "white"
+      )
+      .attr("width", (d) => d.x1 - d.x0)
+      .attr("height", (d) => d.y1 - d.y0);
 
-  node
-    .append("text")
-    .attr("clip-path", (d) => d.clipUid)
-    .selectAll("tspan")
-    .data((d) => d.data.name.split(/(?=[A-Z][^A-Z])/g).concat(format(d.value)))
-    .join("tspan")
-    .attr("fill-opacity", (d, i, nodes) =>
-      i === nodes.length - 1 ? 0.7 : null
-    )
-    .text((d) => d);
+    node
+      .append("text")
+      .attr("clip-path", (d) => `url(#clip-${d.data.name})`)
+      .selectAll("tspan")
+      .data((d) => d.data.name.split(/(?=[A-Z][^A-Z])/g).concat(format(d.value)))
+      .join("tspan")
+      .attr("fill-opacity", (d, i, nodes) =>
+        i === nodes.length - 1 ? 0.7 : null
+      )
+      .text((d) => d);
 
-  node
-    .filter((d) => d.children)
-    .selectAll("tspan")
-    .attr("dx", 3)
-    .attr("y", 13);
-
-  node
-    .filter((d) => !d.children)
-    .selectAll("tspan")
-    .attr("x", 3)
-    .attr(
-      "y",
-      (d, i, nodes) => `${(i === nodes.length - 1) * 0.3 + 1.1 + i * 0.9}em`
-    );
     document.body.appendChild(svg.node());
   })
   .catch((error) => console.error("Error loading JSON data:", error));
