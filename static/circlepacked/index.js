15b62d0dd6cbaa6eb2c87f70f6bd0b1906114ad8
Daniel Ellis <daniel.ellis@ext.esa.int>
2024-12-02 22:06:16 +0000

    development

--- static/circlepacked/index.js
+++ static/circlepacked/index.js
@@ -147,7 +147,7 @@ function chart(data) {
 
   function zoom(event, d) {
 
-    if (d.depth > 1 && d.parent) {d = d.parent}
+    if (d.depth > 2 && d.parent) {d = d.parent}
     else{return 0};
 
     const focus0 = focus;
