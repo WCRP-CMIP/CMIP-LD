1bd188d50e60dd2cc93f73799b9ed53bdd41a083
Daniel Ellis <daniel.ellis@ext.esa.int>
2025-02-14 19:56:40 +0100

    development

--- cmipld/tests/jsonld/__init__.py
+++ cmipld/tests/jsonld/__init__.py
@@ -5,3 +5,7 @@ from pydantic import BaseModel, field_validator
 def field_test(extended_class):
     # dynamic class
     return type(extended_class.__name__, (BaseModel, extended_class), {})
+
+def multi_field_test(args):
+    assert isinstance(args, list)
+    return type('multi_field_test', (BaseModel, *args), {})
